aaef93b04396715ae2190dac9d1603017db6e1df
prudviraj12 <prudvirajreddy98@gmail.com>
2025-09-16 22:21:37 +0530

    2nd one

--- exer.c
+++ exer.c
@@ -1,33 +1,65 @@
 #include <stdio.h>
+#include <math.h>
+
 int main() {
-    int a, b, choice;
-    printf("Enter any number a: ");
-    scanf("%d", &a);
-    printf("Enter any number b: ");
-    scanf("%d", &b);
+//#include <stdio.h>
 
-    printf("Enter which operator:\n");
-    printf("1. sum (a+b)\n");
-    printf("2. sub (a-b)\n");
-    printf("3. mult (a*b)\n");
-    printf("4. div (a/b)\n");
-    scanf("%d", &choice);
+//int main() {
+//	1st one
 
-    if(choice == 1)
-        printf("sum: %d", a + b);
-    else if(choice == 2)
-        printf("sub: %d", a - b);
-    else if(choice == 3)
-        printf("mult: %d", a * b);
-    else if(choice == 4) {
-        if(b != 0)
-            printf("div: %d", a / b);
-        else
-            printf("Error: Division by zero!");
-    }
-    else
-        printf("Invalid choice");
+//   int a, b, choice;
+//
+//
+//    printf("Enter any number a: ");
+//    scanf("%d", &a);
+//    printf("Enter any number b: ");
+//    scanf("%d", &b); 
+//
+//    printf("Enter which operator:\n");
+//    printf("1. sum (a+b)\n");
+//    printf("2. sub (a-b)\n");
+//    printf("3. mult (a*b)\n");
+//    printf("4. div (a/b)\n");
+//    scanf("%d", &choice);
+//
+//    if(choice == 1)
+//        printf("sum: %d", a + b);
+//    else if(choice == 2)
+//        printf("sub: %d", a - b);
+//    else if(choice == 3)
+//        printf("mult: %d", a * b);
+//    else if(choice == 4) {
+//        if(b != 0)
+//            printf("div: %d", a / b);
+//        else
+//            printf("Error: Division by zero!");
+//    }
+//    else
+//        printf("Invalid choice");
+        
+//        2nd one
+
     
+
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    double d = (b * b) - (4 * a * c);
+    if (d == 0) {
+        double r = -b / (2.0 * a);
+        printf("Real and Equal\n");
+        printf("root = %.2f", r);
+    } else if (d > 0) {
+        double r1 = (-b + sqrt(d)) / (2.0 * a);
+        double r2 = (-b - sqrt(d)) / (2.0 * a);
+        printf("Real and Different\n");
+        printf("root1 = %.2f, root2 = %.2f", r1, r2);
+    } else {
+        double real = -b / (2.0 * a);
+        double imag = sqrt(-d) / (2.0 * a);
+        printf("Imaginary\n");
+        printf("root1 = %.2f+%.2fi, root2 = %.2f-%.2fi", real, imag, real, imag);
+    }
     return 0;
 }
+
 
